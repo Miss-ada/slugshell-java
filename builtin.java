ed24bf54b672c4d41002b777adaa753d6ecd572c
katrina95 <34797724+katrina95@users.noreply.github.com>
2018-05-15 12:17:09 -0700

    Update builtin.java

--- builtin.java
+++ builtin.java
@@ -1,116 +1,143 @@
-import jdk.jshell.*;
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class builtin {
+	/**
+	 * * A:获取功能
+		* public String getAbsolutePath()：获取绝对路径
+		* public String getPath():获取路径
+		* public String getName():获取名称
+		* public long length():获取长度。字节数
+		* public long lastModified():获取最后一次的修改时间，毫秒值
+		* public String[] list():获取指定目录下的所有文件或者文件夹的名称数组
+		* public File[] listFiles():获取指定目录下的所有文件或者文件夹的File数组 
+	 */
+	static File dir = new File("/Users/wentingzhu");
+	static File[] list = (new File("/Users/wentingzhu")).listFiles();
+	
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		ArrayList<String> history = new ArrayList<>();
+		
+		System.out.println("Welcom! What can I do for you?");
+		boolean over = false;
+		while(true) {
+			System.out.print("slugshell>> ");
+			String string = sc.nextLine();
+			String[] commend  = string.split("\\u007C");
+			for (String subString : commend) {
+				over = singleCommend(history, over, subString.trim());
+			}
+			if(over) break;
+			history.add(string);
+		}
+ 	}
 
-    }
-    boolean isBuiltin(Command command) {
-        String str = command.name;
-        if (str.equals("exit") || str.equals("history") || str.equals("status") || str.equals("cd") || str.equals("ls")) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    boolean tryBuiltin(Command command) {
-        String str = command.name;
-        if (str.equals("exit")) {
-            return exit(command);
-        } else if (str.equals("ls")){
-            return ls(command);
-        } else if (str.equals("history")) {
-            return history(command);
-        } else if (str.equals("cd")) {
-            return cd(command);
-        } else if (str.equals("status")) {
-            return status(command);
-        } else {
-            return false;
-        }
-        return true;
-    }
-    boolean exit(Command command) throws java.io.IOException, java.lang.InterruptedException{
-        // Get runtime
-        Runtime rt = Runtime.getRuntime();
-        // Start a new process: UNIX command ls
-        String cmd = "exit";
-        //execute the command
-        Process p = rt.exec(cmd);
-        return true;
-    }
-    boolean history(Command command) throws java.io.IOException, java.lang.InterruptedException {
-        // Get runtime
-        java.lang.Runtime rt = java.lang.Runtime.getRuntime();
-        // Start a new process: UNIX command ls
-        java.lang.Process p = rt.exec("history");
-        // You can or maybe should wait for the process to complete
-        p.waitFor();
-        // Get process' output: its InputStream
-        java.io.InputStream is = p.getInputStream();
-        java.io.BufferedReader reader = new java.io.BufferedReader(new InputStreamReader(is));
-        // And print each line
-        String s = null;
-        while ((s = reader.readLine()) != null) {
-            System.out.println(s);
-        }
-        is.close();
-        if (p.exitValue() == 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    boolean ls(Command command) throws java.io.IOException, java.lang.InterruptedException {
-        java.lang.Runtime rt = java.lang.Runtime.getRuntime();
-        java.lang.Process p = rt.exec("ls");
-        p.waitFor();
-        java.io.InputStream is = p.getInputStream();
-        java.io.BufferedReader reader = new java.io.BufferedReader(new InputStreamReader(is));
-        String s = null;
-        while ((s = reader.readLine()) != null) {
-            System.out.println(s);
-        }
-        is.close();
-        if (p.exitValue() == 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    boolean status(Command command) throws java.io.IOException, java.lang.InterruptedException{
-        java.lang.Runtime rt = java.lang.Runtime.getRuntime();
-        java.lang.Process p = rt.exec("history");
-        p.waitFor();
-        java.io.InputStream is = p.getInputStream();
-        java.io.BufferedReader reader = new java.io.BufferedReader(new InputStreamReader(is));
-        String s = null;
-        String cd = "";
-        while ((s = reader.readLine()) != null) {
-            cd = s;
-        }
-        System.out.println(cd);
-        is.close();
-        if (p.exitValue() == 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    boolean cd(Command command) throws java.io.IOException, java.lang.InterruptedException{
-        Runtime rt = Runtime.getRuntime();
-        String arg = command.argument;
-        if (argument == null || argument.equals("")) {
-            System.out.println("input error");
-        }
-        String[] cmd = {"cd",arg};
-        Process p = rt.exec(cmd);
-        return true;
+	private static boolean singleCommend(ArrayList<String> history, boolean over, String string) {
+		if(string.equals("ls")) {
+			list();
+		}else if(string.startsWith("ls *")){
+			list(string);
+		}else if(string.equals("history")) {
+			history(history);
+		}else if(string.equals("status")){
+			System.out.println(history.get(history.size()-1));
+		}else if(string.equals("cd ..")) {
+			goback();
+		}else if(string.equals("cd ~")){
+			home();
+		}else if(string.startsWith("cd ")) {
+			cd(string);
+		}else if(string.startsWith("mv ") || string.startsWith("cp ")){
+			change(string);
+		}else if(string.equals("pwd")) {
+			System.out.println(dir);
+		}else if(string.equals("quit")){
+			System.out.println("See you！");
+			over = true;
+		}
+		return over;
+	}
+
+	private static void change(String string) {
+		String[] fs = string.substring(3).split(" ");
+		if(fs.length != 2) {
+			System.out.println("You should input like: mv/cp aFile bFile");
+		}else {
+			File original = new File(dir, fs[0]);
+			File newFile = new File(dir,fs[1]);
+			System.out.println(original.renameTo(newFile));
+			if(string.startsWith("cp "))copyFile(newFile , original);
+		}
+	}
+
+	private static void history(ArrayList<String> history) {
+		for (int i = 0; i < history.size(); i++) {
+			System.out.println(i + 1 + "." + history.get(i));
+		}
+	}
+	
+	public static void copyFile(File fromFile,File toFile){
+        try {
+        	FileInputStream ins = new FileInputStream(fromFile);
+            FileOutputStream out = new FileOutputStream(toFile);
+            byte[] b = new byte[1024];
+            int n=0;
+            while((n=ins.read(b))!=-1){
+                out.write(b, 0, n);
+            }
+            
+            ins.close();
+            out.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
     }
 
+	private static void home() {
+		dir = new File("/Users/wentingzhu");
+		list = (new File("/Users/wentingzhu")).listFiles();
+	}
+
+
+	private static void list(String string) {
+		for (File file : list) {					//获取文件对象
+			if(file.getName().endsWith(string.substring(4)))System.out.println(file);
+		}
+	}
+
+	private static void goback() {
+		try {
+			dir = dir.getParentFile();
+			list = dir.listFiles();
+		} catch (Exception e) {
+			System.out.println("It is root now");
+			dir = new File("/Users/wentingzhu");
+		}
+	}
+
+	private static void cd(String string) {
+		String cd  = string.substring(3);
+		for(File subFile : list) {
+			if(subFile.isFile())continue;
+			if(subFile.getName().equals(cd)) {
+				dir = subFile;
+				list = dir.listFiles();
+				return;
+			}
+		}
+		System.out.println("Folder doesn't exist!");
+	}
+
+	private static void list() {	
+		for (File file : list) {					//获取文件对象
+			System.out.println(file);
+		}
+	}
 }
